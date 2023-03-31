f965154adcfbbcfee3bfe0c4314c2c9575792d65
Rhys Paulino <rhys.paulino@vanarts.com>
2023-03-31 11:59:46 -0700

    first ng Website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { SideSectionComponent } from './side-section/side-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    MainSectionComponent,
+    SideSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/side-section/side-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-section',
+  templateUrl: './side-section.component.html',
+  styleUrls: ['./side-section.component.scss']
+})
+export class SideSectionComponent {
+
+}
